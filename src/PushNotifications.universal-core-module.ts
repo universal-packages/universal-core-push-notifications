e2a18e2ef4df4088e05323effd99dcd3a8a3adf2
David De Anda <omarandstuff@gmail.com>
2024-03-28 12:15:31 -0600

    Upgrade to new logger usage

--- src/PushNotifications.universal-core-module.ts
+++ src/PushNotifications.universal-core-module.ts
@@ -1,6 +1,7 @@
 import { CoreModule } from '@universal-packages/core'
-import { TerminalTransport } from '@universal-packages/logger'
 import { PushNotifications, PushNotificationsOptions } from '@universal-packages/push-notifications'
+
+import { LOG_CONFIGURATION } from './LOG_CONFIGURATION'
 
 export default class PishNotificationsModule extends CoreModule<PushNotificationsOptions> {
   public static readonly moduleName = 'push-notifications-module'
@@ -9,28 +10,42 @@ export default class PishNotificationsModule extends CoreModule<PushNotification
   public subject: PushNotifications
 
   public async prepare(): Promise<void> {
-    const terminalTransport = this.logger.getTransport('terminal') as TerminalTransport
-    terminalTransport.options.categoryColors['PN'] = 'PURPLE'
-
     this.subject = new PushNotifications(this.config)
 
     this.subject.on('warning', (event: any): void => {
       const { message } = event
 
-      this.logger.publish('WARNING', message, null, 'PN')
+      this.logger.log({ level: 'WARNING', message, category: 'PUSH-NOTIFICATIONS' }, LOG_CONFIGURATION)
     })
 
     this.subject.on('error', (event: any): void => {
       const { error, payload } = event
       const { capability, notification, token } = payload
 
-      this.logger.publish('ERROR', `Error pushing notification [${capability}]`, null, 'PN', { error, metadata: { notification, token } })
+      this.logger.log(
+        {
+          level: 'ERROR',
+          title: `Error pushing notification [${capability}]`,
+          category: 'PUSH-NOTIFICATIONS',
+          error,
+          metadata: { notification, token }
+        },
+        LOG_CONFIGURATION
+      )
     })
 
     this.subject.on('push', (event: any): void => {
       const { capability, notification, token } = event.payload
 
-      this.logger.publish('INFO', `Notification pushed [${capability}]`, null, 'PN', { metadata: { notification, token } })
+      this.logger.log(
+        {
+          level: 'INFO',
+          title: `Notification pushed [${capability}]`,
+          category: 'PUSH-NOTIFICATIONS',
+          metadata: { notification, token }
+        },
+        LOG_CONFIGURATION
+      )
     })
 
     this.subject.prepare()
--- src/PushNotifications.universal-core-task.ts
+++ src/PushNotifications.universal-core-task.ts
@@ -2,6 +2,8 @@ import { CoreTask } from '@universal-packages/core'
 import { populateTemplates } from '@universal-packages/template-populator'
 import path from 'path'
 
+import { LOG_CONFIGURATION } from './LOG_CONFIGURATION'
+
 export default class PushNotificationsTask extends CoreTask {
   public static readonly taskName = 'push-notifications-task'
   public static readonly description = 'Push notifications related tasks'
@@ -10,7 +12,7 @@ export default class PushNotificationsTask extends CoreTask {
     switch (this.directive) {
       case 'init':
         await populateTemplates(path.resolve(__dirname, 'template'), './src', { override: this.args.f })
-        this.logger.publish('INFO', 'Push notifications template initialized')
+        this.logger.log({ level: 'INFO', title: 'Push notifications template initialized' }, LOG_CONFIGURATION)
         break
       default:
         throw new Error(`Unrecognized directive ${this.directive}`)
